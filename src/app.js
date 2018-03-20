4f281c13a20f3be98424d22280f277496792ae3b
lemus <lemuxx55@gmail.com>
2018-03-20 09:00:28 -0600

    Primer

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//Incluye dependencias
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser =  require('body-parser');
+
+//Inicializa express
+const app = express();
+
+//configurackion del puerto
+app.set('port', process.env.PORT || 3000);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//routes
+require('./routes/userRoutes')(app);
+
+app.listen(app.get('port'), () => {
+	console.log('server on port 3000');
+});
